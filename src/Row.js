c031f89ea4e32d375ee4d75690e6313f95f370eb
Devarsh Mavani <devarshmavani19@gmail.com>
2022-10-18 11:19:21 +0530

    Init

--- src/Row.js
+++ src/Row.js
@@ -1,33 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import axios from './axios';
-import requests from './requests';
-import './Row.css';
+import React, { useState, useEffect } from 'react'
+import axios from './axios'
+import requests from './requests'
+import './Row.css'
 
-const base_url = "https://image.tmdb.org/t/p/original/";
+const base_url = 'https://image.tmdb.org/t/p/original/'
 
-function Row({title, fetchUrl, isLargeRow}) {
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [])
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row_posters">
-                {
-                    movies.map(movie => {
-                        return <img className={`row_poster ${isLargeRow && "row_posterLarge"}`} key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
-                    })
-                }  
-            </div>
-
-        </div>
-    )
+function Row ({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    async function fetchData () {
+      const request = await axios.get(fetchUrl)
+      setMovies(request.data.results)
+      return request
+    }
+    fetchData()
+  }, [])
+  return (
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className='row_posters'>
+        {movies.map(movie => {
+          return (
+            <img
+              className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+              key={movie.id}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={'Best Netflix Movies'}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default Row;
+export default Row
